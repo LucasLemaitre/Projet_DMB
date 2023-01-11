0baf2441cff4a79d12a8be692a9176969a39bcc2
Jean-Loup MOLL <jeanloup65@hotmail.fr>
2023-01-11 21:10:12 +0100

    En train de reformater le csv

--- src/main/scala/Anime.scala
+++ src/main/scala/Anime.scala
@@ -8,7 +8,7 @@ object anime_stats extends App {
 
   // Récupération des données
   val DATA_PATH = "anime.csv"
-  val caract_rdd = sc.textFile(DATA_PATH).map(row => row.replace(';', '!').split(','))
+  val caract_rdd = sc.textFile(DATA_PATH).map(row => deal_with_quotes(row.replace(';', '!')).split(','))
   val headerRow = caract_rdd.first()
   var caract_rdd_clean = caract_rdd.filter(row => row(0) != headerRow(0))
 
@@ -39,17 +39,45 @@ object anime_stats extends App {
   val col_plan_to_watch = headerRow.indexOf("Plan to Watch")
   val col_score_amount = new Array[Int](10);
   for (i <- 0 to 9) {
-    col_score_amount(i) = headerRow.indexOf("Score-" + i+1)
+    col_score_amount(i) = headerRow.indexOf("Score-" + (i+1))
   }
 
   // Calcul de la médiane
-  val mediane = caract_rdd_clean.map(
+  val rdd_score_median = caract_rdd_clean.map(
     anime => (
       anime(col_anime_id),
-      med(col_score_amount.map(i_col => anime(i_col).toFloat))
+      anime(col_name),
+      anime(col_rating).toFloat,
+      med(col_score_amount.map(
+        i_col => anime(i_col).toFloat
+      ))
     )
   )
 
+
+
+
+
+  // Utilisation
+  println("Please enter the id of an anime :")
+  val anime_id_input = scala.io.StdIn.readLine()
+  println("The median for this anime is:")
+  println(rdd_score_median.filter(row => row._1 == anime_id_input).first()._4)
+
+  // On prend les 10 meilleurs moyennes (TODO? pour les animes notés au moins 1000 fois)
+  val best_med = rdd_score_median.sortBy(
+    (row=>row._3)
+  ).take(10)
+
+  println("Top 10 best scores:")
+  println(best_med.mkString("\n"))
+
+
+
+
+
+  // This function takes for an anime an array A st A(i) is the amount of people who put a score of (i+1) for this anime.
+  // It returns the median score.
   def med(amounts: Array[Float]): Int = {
     val half_amount = amounts.sum / 2
 
@@ -62,4 +90,28 @@ object anime_stats extends App {
     }
     return 0
   };
+
+  //This function takes a String s and replace every substring of the form: "_,_,_" with a new one of the form: _/_/_
+  def deal_with_quotes(s: String): String = {
+    var res = s
+
+    var i_next_quote = res.indexOf("\"")
+
+    var substr = ""
+    var i_end_substr = -1
+    while(i_next_quote != -1){
+      //We suppose quotes come in pairs ("_")
+      substr = res.substring(i_next_quote+1)
+      i_end_substr = substr.indexOf("\"")
+      substr = substr.substring(0,i_end_substr)
+
+      res = res.substring(0,i_next_quote) +
+        substr.replace(",","/") +
+        res.substring(i_next_quote + substr.length + 2) // +2 for the quotes
+
+      i_next_quote = res.indexOf("\"")
+    }
+
+    return res
+  }
 }
